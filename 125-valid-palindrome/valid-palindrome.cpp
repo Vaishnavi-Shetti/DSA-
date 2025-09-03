5511e23cc0631ccd7043413bf9746d2d105bf0c8
Vaishnavi Shetti <152868060+Vaishnavi-Shetti@users.noreply.github.com>
2025-09-03 15:42:30 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (94.15%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
     bool isPalindrome(string str) {
-        str.erase(remove_if(str.begin(),str.end(),[](char c){return !isalnum(c);}),str.end());
-        
-        transform(str.begin(),str.end(),str.begin(),::tolower);
-        int n=str.size();
-        int s=0;
-        int e=n-1;
-        while(s<=e){
-            if(str[s]!=str[e]){
-                return false;
-            }
+       int s=0;
+       int e=str.size()-1;
+       while(s<e){
+        if(!isalnum(str[s])) {
             s++;
-            e--;
+            continue;
         }
+        if(!isalnum(str[e])) {
+            e--;
+            continue;
+        }
+        if(tolower(str[s])!= tolower(str[e])){
+            return false;
+        }
+        s++;
+        e--;
+       }
+
         return true;
     }
 };
